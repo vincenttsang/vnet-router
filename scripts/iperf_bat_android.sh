a32e6bb0add4c411b46fa98b0512ee2c782c0f10
Vincent Tsang <yukun.zeng.hit@outlook.com>
2017-05-05 11:32:37 -0500

    got rid of the value extraction part, use raw output for writing to the log

--- scripts/iperf_bat_android.sh
+++ scripts/iperf_bat_android.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 # for android
-echo "Bandwidth Jitter PL/Total PLR" >> log
+rm -f log
+echo "# TimeInterval TotalBytes Bandwidth Jitter PL/Total PLR" >> log
 # 1st param: the ip of iperf server
 # 2nd param: starting transferring rate
 i=$(($2))
@@ -25,26 +26,30 @@ do
 	# TODO be sure to replace by using the exact path to iperf when running on Android
 	# for example, /data/local/tmp/iperf instead of iperf
 	# TODO if use server, we should use 11p instead of 12p
-        result="$(/data/local/tmp/iperf -u -c $1 -b ${speed}mbit -t ${time}s | sed -n 12p)"
+        result="$(/data/local/tmp/iperf -u -c $1 -b ${speed}mbit -t ${time}s | grep " ms")"
 	# sleep until the last UDP connection timed out
 	sleep 2
 	echo "Raw Results:"
         echo $result
+        #result="$(echo $result | tr -s ' ')"
+        #echo "Raw Results After Getting Rid of Spaces:"
+        #echo $result
         # TODO change the index -f[x] to -f[7-13] if using experiment time over 10
 	# otherwise use -f[8-14]
-        bandwidth="$(echo $result | cut -d' ' -f7 )"
-        jitter="$(echo $result | cut -d' ' -f9)"
-        # packet loss
-        pl="$(echo $result | cut -d' ' -f11)"
-        total="$(echo $result | cut -d' ' -f12)"
-        plr="$(echo $result | cut -d' ' -f13)"
-        tmpAllInfo="$bandwidth ""$jitter ""$pl""$total ""$plr"
+        #bandwidth="$(echo $result | cut -d' ' -f7 )"
+        #jitter="$(echo $result | cut -d' ' -f9)"
+        ## packet loss
+        #pl="$(echo $result | cut -d' ' -f11)"
+        #total="$(echo $result | cut -d' ' -f12)"
+        #plr="$(echo $result | cut -d' ' -f13)"
+        #tmpAllInfo="$bandwidth ""$jitter ""$pl""$total ""$plr"
+        tmpAllInfo=$result
         echo "${tmpAllInfo}" >> log
         #allInfo="${tmpAllInfo}\n${allInfo}"
         #echo "AllInfo:"
         #echo -e "$allInfo"
-        echo "Saved Results:"
-        echo $tmpAllInfo
+        #echo "Saved Results:"
+        #echo $tmpAllInfo
 
 	j=$(($j + 1))
     done
--- scripts/iperf_bat_android_vm.sh
+++ scripts/iperf_bat_android_vm.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
-# for server
-rm -f log
-echo "# TimeInterval TotalBytes Bandwidth Jitter PL/Total PLR" >> log
+#!/system/bin/sh
+# for android
+echo "Bandwidth Jitter PL/Total PLR" >> log
+# 1st param: the ip of iperf server
+# 2nd param: starting transferring rate
 i=$(($2))
+# 3rd param: runtime of each experiment
 time=$(($3))
-# iteration of transmitting rate should be 0 to 60 Mbps
+# TODO iteration of transmitting rate should be 0 to 60 Mbps
 while [ $(($i)) -le 65 ];
 do
     speed="$i""Mbits/sec"
@@ -16,37 +18,33 @@ do
     echo $speed >> log
 
     j=$((1))
-    # here we can repetition times of redundant experiments
+    # TODO here we can repetition times of redundant experiments
     while [ $(($j)) -le 5 ];
     do
     	# TODO the test interval shall be larger in real experiments, e.g., 30s
-    	# TODO the ip addr should be changed and also before each test, restart the iperf
-	# server by control+c and rerun iperf -u -s
 	# TODO be sure to replace by using the exact path to iperf when running on Android
 	# for example, /data/local/tmp/iperf instead of iperf
-        #iperf -u -c 10.0.0.21 -b ${speed}mbit -t 30s | sed -n 11p
 	# TODO if use server, we should use 11p instead of 12p
-        # result="$(/data/local/tmp/iperf -u -c $1 -b ${speed}mbit -t 30s | sed -n 12p)"
-	# TODO for server
-	result="$(iperf -u -c $1 -b ${speed}mbit -t ${time}s | grep " ms")"
+        result="$(/data/local/tmp/iperf -u -c $1 -b ${speed}mbit -t ${time}s | sed -n 11p)"
 	# sleep until the last UDP connection timed out
-	# TODO if use server, sleep 2s, else if use android, sleep 2
-	sleep 2s
+	sleep 2
 	echo "Raw Results:"
         echo $result
         # TODO change the index -f[x] to -f[7-13] if using experiment time over 10
 	# otherwise use -f[8-14]
-        #bandwidth="$(echo $result | cut -d' ' -f7 )"
-        #jitter="$(echo $result | cut -d' ' -f9)"
-        ## packet loss
-        #pl="$(echo $result | cut -d' ' -f11)"
-        #total="$(echo $result | cut -d' ' -f12)"
-        #plr="$(echo $result | cut -d' ' -f13)"
-        #tmpAllInfo="$bandwidth ""$jitter ""$pl""$total ""$plr"
-        echo "${result}" >> log
+        bandwidth="$(echo $result | cut -d' ' -f7 )"
+        jitter="$(echo $result | cut -d' ' -f9)"
+        # packet loss
+        pl="$(echo $result | cut -d' ' -f11)"
+        total="$(echo $result | cut -d' ' -f12)"
+        plr="$(echo $result | cut -d' ' -f13)"
+        tmpAllInfo="$bandwidth ""$jitter ""$pl""$total ""$plr"
+        echo "${tmpAllInfo}" >> log
         #allInfo="${tmpAllInfo}\n${allInfo}"
         #echo "AllInfo:"
         #echo -e "$allInfo"
+        echo "Saved Results:"
+        echo $tmpAllInfo
 
 	j=$(($j + 1))
     done
